996e0ff84a296f0e960e9bc63240ff5c1b8a802e
Andrei Berceanu <andreicberceanu@gmail.com>
2023-05-13 22:17:15 +0300

    Adapt reader to openpmd-api docs

--- opmdtogeant/reader.py
+++ opmdtogeant/reader.py
@@ -1,5 +1,5 @@
-import h5py
-from openpmd_api import Series, Access_Type, Iteration_Encoding
+import openpmd_api as io
+
 
 class HDF5Reader:
     def __init__(self, file_path):
@@ -7,28 +7,35 @@ class HDF5Reader:
 
     def read_file(self):
         try:
-            series = Series(self.file_path, Access_Type.read_only)
+            series = io.Series(self.file_path, io.Access.read_only)
 
             # Get the last iteration
             it = series.iterations[sorted(series.iterations.keys())[-1]]
 
             # Get the electrons particle species
-            electrons = it.particles['electrons']
+            electrons = it.particles["electrons"]
 
             # Position and momenta data
-            position_x = electrons['position']['x'][:]
-            position_y = electrons['position']['y'][:]
-            position_z = electrons['position']['z'][:]
+            position_x = electrons["position"]["x"][:]
+            position_y = electrons["position"]["y"][:]
+            position_z = electrons["position"]["z"][:]
 
-            momenta_px = electrons['momentum']['x'][:]
-            momenta_py = electrons['momentum']['y'][:]
-            momenta_pz = electrons['momentum']['z'][:]
+            momenta_px = electrons["momentum"]["x"][:]
+            momenta_py = electrons["momentum"]["y"][:]
+            momenta_pz = electrons["momentum"]["z"][:]
 
             # Weight data
-            weights = electrons['weighting'][:]
+            weights = electrons["weighting"][:]
 
-            return position_x, position_y, position_z, momenta_px, momenta_py, momenta_pz, weights
+            return (
+                position_x,
+                position_y,
+                position_z,
+                momenta_px,
+                momenta_py,
+                momenta_pz,
+                weights,
+            )
 
         except Exception as e:
             print(f"An error occurred while reading the file: {e}")
-
