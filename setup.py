d4b3cf2edcd17a2ec18602cbac4017bf8c006cc3
Andrei Berceanu <andreicberceanu@gmail.com>
2023-05-13 20:02:34 +0300

    Update package description and requirements

--- setup.py
+++ setup.py
@@ -3,20 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name="opmdtogeant",
     version="0.1.0",
-    description="A Python package to read, parse, merge, and write data from HDF5 files produced by PIConGPU",
+    description="A Python package to export particle data from openPMD to text format",
     author="Andrei Berceanu",
     author_email="andreicberceanu@gmail.com",
     packages=find_packages(),
+    python_requires='==3.11.*',
     install_requires=[
-        "openpmd-api==0.14.2",
-        "h5py==3.3.0",
-        "numpy==1.21.2",
+        "openpmd-api==0.15.*",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.11",
     ],
 )
-
