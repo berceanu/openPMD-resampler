f546ede8f598904a33cbde1aa356dfe81846bb50
Andrei Berceanu <andreicberceanu@gmail.com>
2023-07-11 19:23:49 +0300

    Use data export module

--- usage.py
+++ usage.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from openpmd_viewer.addons import LpaDiagnostics
 
 from opmdtogeant.reader import HDF5Reader, electron_mass_MeV_c2
+from opmdtogeant.df_to_txt import DataFrameToFile
 
 
 def main():
@@ -17,12 +18,16 @@ def main():
     h5_reader = HDF5Reader(str(h5_path))
     df = h5_reader.read_file()
 
+    # Write the dataframe to a file
+    df_to_file = DataFrameToFile(df)
+    df_to_file.write_to_file(h5_path.with_suffix(".txt"))
+
     # Sanity check
     diags = LpaDiagnostics(str(h5_path), check_all_files=False)
     # a = diags.get_energy_spread(iteration=126175, species="e_all", property="energy")
     mean_gamma = diags.get_mean_gamma(iteration=126175, species="e_all")[0]
     mean_gamma_mev = mean_gamma * electron_mass_MeV_c2
-    print(f"\nopenPMD-viewer's (weighted) mean energy is {mean_gamma_mev:.6e} MeV.")
+    print(f"\nopenPMD-viewer's (weighted) mean energy is {mean_gamma_mev:.6e} MeV.\n")
 
 
 if __name__ == "__main__":
