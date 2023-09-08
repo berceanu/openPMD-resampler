e52a4429b28be60c911c477fd3746a339bb5ab96
Andrei Berceanu <andreicberceanu@gmail.com>
2023-09-08 21:54:45 +0300

    Refactor PhaseSpaceVisualizer into smaller classes

--- opmdresampler/visualize_phase_space.py
+++ opmdresampler/visualize_phase_space.py
@@ -1,117 +1,88 @@
 import itertools
 import os
-from typing import Optional, Tuple
+from abc import ABC, abstractmethod
+from typing import Optional
 
-import pandas as pd
 import numpy as np
+import pandas as pd
+from matplotlib.cm import ScalarMappable
 from matplotlib.colors import LogNorm
 
-from opmdresampler.figure_layout import FigureCreator
+from opmdresampler.figure_layout import FigureLayout
+from opmdresampler.histograms import EqualWeightDistributionPlot, StandardHistogramPlot
+from opmdresampler.image_plots import StandardDataShaderPlot
+from opmdresampler.plot_utils import PURPLE_RABBIT
 from opmdresampler.utils import combine_images, unique_filename
 
 
-class PhaseSpaceVisualizer:
+class MultiplePanelPlotter(ABC):
+    layout = None
+    weight_col = "weights"
+
     def __init__(
         self,
-        dataframe: pd.DataFrame,
-        weight_column: str = "weights",
-        energy_column: str = "energy_mev",
-        energy_label: Optional[str] = None,
-        vmax: Optional[float] = None,
+        df,
+        position_features,
+        position_labels,
+        momentum_features,
+        momentum_labels,
+        output_filename=None,
     ):
-        self.dataframe = dataframe
-        self.position_features = ("position_x_um", "position_y_um", "position_z_um")
-        self.momentum_features = (
-            "momentum_x_mev_c",
-            "momentum_y_mev_c",
-            "momentum_z_mev_c",
-        )
-        self.position_labels = (r"$x$ ($\mu$m)", r"$y$ ($\mu$m)", r"$z$ ($\mu$m)")
-        self.momentum_labels = (
-            r"$p_{\mathrm{x}}$ (MeV/c)",
-            r"$p_{\mathrm{y}}$ (MeV/c)",
-            r"$p_{\mathrm{z}}$ (MeV/c)",
-        )
-        self.weight_column = weight_column
-        self.energy_column = energy_column
+        self.fig_layout = None
+        self.df = df
+        self.position_features = position_features
+        self.position_labels = position_labels
+        self.momentum_features = momentum_features
+        self.momentum_labels = momentum_labels
 
-        if self.energy_column == "energy_mev":
-            self.energy_label = "Energy (MeV)"
-        elif energy_label is not None:
-            self.energy_label = energy_label
+        if output_filename is not None:
+            self.output_filename = output_filename
         else:
-            raise ValueError("Must supply energy_label.")
+            self.output_filename = unique_filename(".png")
 
-        if vmax is None:
-            vmax = self.calculate_vmax()
-        self._vmax = vmax
+    @abstractmethod
+    def plot_panels(self):
+        """
+        Abstract method to plot panels. This method needs to be implemented in each child class.
+        One needs to loop through the position and momentum features and use the plotter on each panel.
+        """
 
-        self.norm = LogNorm(
-            vmin=self.dataframe[self.weight_column].min(), vmax=self.vmax
-        )
+    def create_plot(self):
+        self.fig_layout = FigureLayout(layout=self.layout)
+        self.plot_panels()
+        return self
 
-    def calculate_vmax(self):
-        bin_edges = np.histogram_bin_edges(self.dataframe["momentum_x_mev_c"], bins=200)
-        counts, _ = np.histogram(
-            self.dataframe["momentum_x_mev_c"],
-            bins=bin_edges,
-            weights=self.dataframe["weights"],
-        )
-        order_of_magnitude = np.floor(np.log10(counts.max()))
-        ten_to_the_order = np.power(10, order_of_magnitude)
-        return ten_to_the_order
+    def savefig(self):
+        self.fig_layout.savefig(self.output_filename)
+        return self.output_filename
 
-    @property
-    def vmax(self):
-        return self._vmax
 
-    @vmax.setter
-    def vmax(self, value):
-        self._vmax = value
+class MultipleHistogramPlotter(MultiplePanelPlotter):
+    layout = (3, 3, 2)
+    energy_col = "energy_mev"
+    energy_label = "Energy (MeV)"
 
-    def save_comparative_fig(
-        self, other, output_filename: str = "plots/comparative_phase_space.png"
-    ):
-        if not isinstance(other, PhaseSpaceVisualizer):
-            raise TypeError("Both objects must be instances of PhaseSpaceVisualizer")
-
-        if set(self.dataframe.columns) != set(other.dataframe.columns):
-            raise ValueError("Dataframes must have exactly the same column names.")
-
-        filenames = [
-            self.plot_bunch(title=("original", "royalblue")),
-            other.plot_bunch(title=("reduced", "#FF7F0E")),
-            self.plot_histograms(other),
-            self.plot_emittance(title=("original", "royalblue")),
-            other.plot_emittance(title=("reduced", "#FF7F0E")),
-        ]
-
-        # Combine the plots
-        combine_images(filenames, output_filename)
-
-        # Delete the temporary files
-        for fname in filenames:
-            os.remove(fname)
-
-    def savefig(self, output_filename: str = "plots/phase_space.png"):
-        filenames = [self.plot_bunch(), self.plot_histograms(), self.plot_emittance()]
-        combine_images(filenames, output_filename)
-        for fname in filenames:
-            os.remove(fname)
-
-    def plot_histograms(
+    def __init__(
         self,
-        other: Optional["PhaseSpaceVisualizer"] = None,
-        output_filename: Optional[str] = None,
+        df,
+        position_features,
+        position_labels,
+        momentum_features,
+        momentum_labels,
+        output_filename=None,
     ):
-        if other is not None and not isinstance(other, PhaseSpaceVisualizer):
-            raise TypeError("Both objects must be instances of PhaseSpaceVisualizer")
+        super().__init__(
+            df,
+            position_features,
+            position_labels,
+            momentum_features,
+            momentum_labels,
+            output_filename,
+        )
+        self.all_plotters = []
 
-        if output_filename is None:
-            output_filename = unique_filename(".png")
-
-        fig = FigureCreator(layout=[3, 3, 2])
-
+    def plot_panels(self):
+        self.all_plotters = []
         # first two rows, 3 columns each
         for row, (feature_set, label_set) in enumerate(
             zip(
@@ -120,72 +91,128 @@ class PhaseSpaceVisualizer:
             )
         ):
             for col, feature in enumerate(feature_set):
-                use_y_label = col == 0
-                fig.histogram_plot(
-                    ax_position=(row, col),
-                    df=self.dataframe,
-                    col=feature,
-                    x_label=label_set[col],
-                    use_y_label=use_y_label,
-                    weight_col=self.weight_column,
-                )
-                if other is not None:
-                    ax = fig._get_ax((row, col)).twinx()
-                    fig.histogram_plot(
-                        ax=ax,
-                        df=other.dataframe,
-                        col=feature,
-                        x_label=label_set[col],
-                        weight_col=other.weight_column,
-                    )
+                ax = self.fig_layout.get_ax(row, col)
+                plotter = StandardHistogramPlot(ax, self.df, feature, label_set[col])
+                plotter.weight_col = self.weight_col
+                plotter.create_plot()
+                self.all_plotters.append(plotter)
+                if col != 0:
+                    ax.set_ylabel("")
 
         # last row, 2 columns
-        fig.weight_distribution_plot(
-            ax_position=(2, 0),
-            df=self.dataframe,
-            weight_col=self.weight_column,
-            label="original",
-        )
-        if other is not None:
-            ax = fig._get_ax((2, 0)).twinx()
-            fig.weight_distribution_plot(
-                ax=ax,
-                df=other.dataframe,
-                weight_col=other.weight_column,
-                label="reduced",
+        ax = self.fig_layout.get_ax(2, 0)
+        plotter = EqualWeightDistributionPlot(ax, self.df)
+        plotter.weight_col = self.weight_col
+        plotter.create_plot()
+        self.all_plotters.append(plotter)
+
+        ax = self.fig_layout.get_ax(2, 1)
+        plotter = StandardHistogramPlot(ax, self.df, self.energy_col, self.energy_label)
+        plotter.weight_col = self.weight_col
+        plotter.create_plot()
+        self.all_plotters.append(plotter)
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+
+        attributes_to_check = [
+            "layout",
+            "weight_col",
+            "energy_col",
+            "energy_label",
+            "position_features",
+            "position_labels",
+            "momentum_features",
+            "momentum_labels",
+        ]
+
+        for attr in attributes_to_check:
+            if getattr(self, attr) != getattr(other, attr):
+                return False
+
+        return True
+
+    def __add__(self, other):
+        if not isinstance(other, type(self)):
+            raise ValueError(f"Can only add another {type(self)} instance.")
+
+        if self != other:
+            raise ValueError(
+                f"The two {type(self)} instances do not have equal attributes."
             )
-        fig.histogram_plot(
-            ax_position=(2, 1),
-            df=self.dataframe,
-            col=self.energy_column,
-            x_label=self.energy_label,
-            weight_col=self.weight_column,
-        )
-        if other is not None:
-            ax = fig._get_ax((2, 1)).twinx()
-            fig.histogram_plot(
-                ax=ax,
-                df=other.dataframe,
-                col=other.energy_column,
-                x_label=other.energy_label,
-                weight_col=other.weight_column,
+
+        if len(self.all_plotters) != len(other.all_plotters):
+            raise ValueError(
+                "The number of elements in 'self.all_plotters' and 'other.all_plotters' are not equal."
             )
-        fig.save_figure(output_filename)
-        return output_filename
 
-    def plot_bunch(
-        self,
-        output_filename: Optional[str] = None,
-        title: Optional[Tuple[str, str]] = None,
-    ):
-        if output_filename is None:
-            output_filename = unique_filename(".png")
+        for self_plotter, other_plotter in zip(self.all_plotters, other.all_plotters):
+            self_plotter += other_plotter
 
-        layout = [3, 3]
-        # Assume both rows have the same number of columns
-        num_cols = layout[0]
-        fig = FigureCreator(layout=layout, title=title)
+        return self
 
+
+class MultipleImagePlotter(MultiplePanelPlotter):
+    cmap = PURPLE_RABBIT
+    cbar_label = "Number of 'real' electrons"
+    norm = None
+
+    def compute_vmax(self):
+        std_hist = StandardHistogramPlot(ax=None, df=self.df, col=self.df.columns[0])
+        std_hist.weight_col = self.weight_col
+        _, density = std_hist.compute_histogram()
+
+        order_of_magnitude = np.floor(np.log10(density.max()))
+        return np.power(10, order_of_magnitude)
+
+    def compute_norm(self):
+        if self.norm is None:
+            vmax = self.compute_vmax()
+            self.norm = LogNorm(vmin=self.df[self.weight_col].min(), vmax=vmax)
+        return self.norm
+
+    def add_colorbar(self):
+        self.fig_layout.fig.subplots_adjust(right=0.91)
+        cax = self.fig_layout.fig.add_axes([0.92, 0.15, 0.025, 0.7])
+
+        # Create a "fake" ScalarMappable with the colormap and norm
+        sm = ScalarMappable(cmap=self.cmap, norm=self.norm)
+        sm.set_array([])  # Make the image invisible
+
+        # Now we can create the colorbar
+        cbar = self.fig_layout.fig.colorbar(sm, cax=cax, orientation="vertical")
+        cbar.set_label(self.cbar_label)
+        return cbar
+
+    def create_plot(self):
+        super().create_plot()
+        self.compute_norm()
+        self.add_colorbar()
+        return self
+
+
+class EmittancePlotter(MultipleImagePlotter):
+    layout = (3, 3, 3)
+
+    def plot_panels(self):
+        for row, (position, position_label) in enumerate(
+            zip(self.position_features, self.position_labels)
+        ):
+            for col, (momentum, momentum_label) in enumerate(
+                zip(self.momentum_features, self.momentum_labels)
+            ):
+                ax = self.fig_layout.get_ax(row, col)
+                plotter = StandardDataShaderPlot(
+                    ax, self.df, position, momentum, position_label, momentum_label
+                )
+                plotter.create_plot(add_cbar=False)
+
+
+class BunchPlotter(MultipleImagePlotter):
+    layout = (3, 3)
+
+    def plot_panels(self):
         coords = {
             coord: (feature, label)
             for coord, feature, label in zip(
@@ -206,49 +233,104 @@ class PhaseSpaceVisualizer:
         axes_mom = ((p["x"], p["y"]), (p["x"], p["z"]), (p["y"], p["z"]))
         combined_axes = itertools.chain(axes_pos, axes_mom)
 
+        num_cols = self.layout[0]
         for i, ((x_col, x_label), (y_col, y_label)) in enumerate(combined_axes):
             row = i // num_cols
             col = i % num_cols
-            fig.shade_plot(
-                ax_position=(row, col),
-                df=self.dataframe,
-                x_col=x_col,
-                y_col=y_col,
-                x_label=x_label,
-                y_label=y_label,
-                weight_col=self.weight_column,
-                norm=self.norm,
+            ax = self.fig_layout.get_ax(row, col)
+            plotter = StandardDataShaderPlot(
+                ax, self.df, x_col, y_col, x_label, y_label
             )
-        fig.create_colorbar(self.norm)
-        fig.save_figure(output_filename)
-        return output_filename
+            plotter.create_plot(add_cbar=False)
 
-    def plot_emittance(
+
+class PhaseSpaceVisualizer:
+    def __init__(
         self,
-        output_filename: Optional[str] = None,
-        title: Optional[Tuple[str, str]] = None,
+        df: pd.DataFrame,
+        weight_col: str = "weights",
+        energy_col: str = "energy_mev",
+        energy_label: Optional[str] = "Energy (MeV)",
     ):
-        if output_filename is None:
-            output_filename = unique_filename(".png")
+        self.df = df
+        self.position_features = ("position_x_um", "position_y_um", "position_z_um")
+        self.momentum_features = (
+            "momentum_x_mev_c",
+            "momentum_y_mev_c",
+            "momentum_z_mev_c",
+        )
+        self.position_labels = (r"$x$ ($\mu$m)", r"$y$ ($\mu$m)", r"$z$ ($\mu$m)")
+        self.momentum_labels = (
+            r"$p_{\mathrm{x}}$ (MeV/c)",
+            r"$p_{\mathrm{y}}$ (MeV/c)",
+            r"$p_{\mathrm{z}}$ (MeV/c)",
+        )
+        self.weight_col = weight_col
+        self.energy_col = energy_col
+        self.energy_label = energy_label
 
-        fig = FigureCreator(layout=[3, 3, 3], title=title)
+        self.bunch_plotter = BunchPlotter(
+            self.df,
+            self.position_features,
+            self.position_labels,
+            self.momentum_features,
+            self.momentum_labels,
+        )
+        self.bunch_plotter.weight_col = self.weight_col
+        #
+        self.emittance_plotter = EmittancePlotter(
+            self.df,
+            self.position_features,
+            self.position_labels,
+            self.momentum_features,
+            self.momentum_labels,
+        )
+        self.emittance_plotter.weight_col = self.weight_col
+        #
+        self.histograms_plotter = MultipleHistogramPlotter(
+            self.df,
+            self.position_features,
+            self.position_labels,
+            self.momentum_features,
+            self.momentum_labels,
+        )
+        self.histograms_plotter.weight_col = self.weight_col
+        self.histograms_plotter.energy_col = self.energy_col
+        self.histograms_plotter.energy_label = self.energy_label
 
-        for row, (position, position_label) in enumerate(
-            zip(self.position_features, self.position_labels)
-        ):
-            for col, (momentum, momentum_label) in enumerate(
-                zip(self.momentum_features, self.momentum_labels)
-            ):
-                fig.shade_plot(
-                    ax_position=(row, col),
-                    df=self.dataframe,
-                    x_col=position,
-                    y_col=momentum,
-                    x_label=position_label,
-                    y_label=momentum_label,
-                    weight_col=self.weight_column,
-                    norm=self.norm,
-                )
-        fig.create_colorbar(self.norm)
-        fig.save_figure(output_filename)
-        return output_filename
+        self.filenames = [
+            self.bunch_plotter.create_plot().savefig(),
+            self.histograms_plotter.create_plot().savefig(),
+            self.emittance_plotter.create_plot().savefig(),
+        ]
+
+    def savefig(self, output_filename: str):
+        # Combine the plots
+        combine_images(self.filenames, output_filename)
+        # Delete the temporary files
+        for fname in self.filenames:
+            os.remove(fname)
+
+    def __add__(self, other):
+        if not isinstance(other, type(self)):
+            raise ValueError(f"Can only add another {type(self)} instance.")
+
+        if set(self.df.columns) != set(other.df.columns):
+            raise ValueError(
+                f"The two {type(self)} instances do not have the same DataFrame column names."
+            )
+
+        # TODO: add plot titles and legend
+        self_hist = self.histograms_plotter.create_plot()
+        other_hist = other.histograms_plotter.create_plot()
+        self_hist += other_hist
+
+        self.filenames = [
+            self.bunch_plotter.create_plot().savefig(),
+            other.bunch_plotter.create_plot().savefig(),
+            self_hist.savefig(),
+            self.emittance_plotter.create_plot().savefig(),
+            other.emittance_plotter.create_plot().savefig(),
+        ]
+
+        return self
