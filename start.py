df122109cc05019b73e37a160d6b4cb58abbe9c1
Andrei Berceanu <andreicberceanu@gmail.com>
2023-10-09 22:42:29 +0300

    Add production script

---
+++ start.py
@@ -0,0 +1,41 @@
+"""
+This module provides a command-line interface for reading OpenPMD files,
+visualizing phase space, resampling particles, and writing the results to a text file.
+"""
+import argparse
+from pathlib import Path
+
+from openpmd_resampler.df_to_txt import DataFrameToFile
+from openpmd_resampler.reader import ParticleDataReader
+from openpmd_resampler.resampling import ParticleResampler
+from openpmd_resampler.visualize_phase_space import PhaseSpaceVisualizer
+
+
+def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("opmd_path", type=str, help="Path to the OpenPMD file")
+    parser.add_argument("--species", "-s", type=str, default="e_all",
+                        help="Particle species name (default: e_all")
+    args = parser.parse_args()
+    opmd_path = Path(args.opmd_path)
+    particle_species_name = args.species
+
+    # Create the dataframe
+    df = ParticleDataReader.from_file(opmd_path, particle_species_name=particle_species_name)
+
+    # Apply thinning algorithm to df, resulting in df_thin
+    resampler = ParticleResampler(df)
+    df_thin = resampler.global_leveling_thinning(k=100).set_weights_to(1).finalize()
+
+    phase_space_thin = PhaseSpaceVisualizer(df_thin, label="Resampled data")
+    phase_space_thin.create_plot().savefig("phase_space.png")
+
+    # Write the reduced dataframe to a file
+    DataFrameToFile(df_thin).exclude_weights().exclude_energy().write_to_file(
+        opmd_path.with_suffix(".txt")
+    )
+
+
+if __name__ == "__main__":
+    main()
