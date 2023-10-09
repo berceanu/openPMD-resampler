1d524ecd44e9613c92a9eb3ffdc113a2ed7fcd32
Andrei Berceanu <andreicberceanu@gmail.com>
2023-10-09 15:30:37 +0300

    Use numpy

--- openpmd_resampler/reader.py
+++ openpmd_resampler/reader.py
@@ -134,7 +134,7 @@ class DataFrameCreator:
 
     def convert_to_SI(self):
         for column_name, unit_SI in self.units.items():
-            self.df[column_name] = self.df[column_name].multiply(unit_SI)
+            self.df[column_name].values *= unit_SI
 
 
 class DataFrameUpdater:
