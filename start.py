9518ea96997b712b0073fcda57b24861930d05c1
Andrei Berceanu <andreicberceanu@gmail.com>
2023-10-09 23:12:13 +0300

    Add flags for printing and saving

--- start.py
+++ start.py
@@ -16,25 +16,36 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("opmd_path", type=str, help="Path to the OpenPMD file")
     parser.add_argument("--species", "-s", type=str, default="e_all",
-                        help="Particle species name (default: e_all")
+                        help="Particle species name (default: e_all)")
+    parser.add_argument("--reduction_factor", "-k", type=int, default=2,
+                        help="The 'k' level for global leveling thinning (default: 2)")
+    parser.add_argument("--no_plot", action="store_true",
+                        help="If set, the phase space plot will not be created.")
+    parser.add_argument("--no_save", action="store_true",
+                        help="If set, the resulting dataframe will not be saved to file.")
+
     args = parser.parse_args()
     opmd_path = Path(args.opmd_path)
     particle_species_name = args.species
+    reduction_factor = args.reduction_factor
+    no_plot = args.no_plot
+    no_save = args.no_save
 
     # Create the dataframe
     df = ParticleDataReader.from_file(opmd_path, particle_species_name=particle_species_name)
 
     # Apply thinning algorithm to df, resulting in df_thin
     resampler = ParticleResampler(df)
-    df_thin = resampler.global_leveling_thinning(k=100).set_weights_to(1).finalize()
+    df_thin = resampler.global_leveling_thinning(k=reduction_factor).set_weights_to(1).finalize()
 
-    phase_space_thin = PhaseSpaceVisualizer(df_thin, label="Resampled data")
-    phase_space_thin.create_plot().savefig("phase_space.png")
+    if not no_plot:
+        phase_space_thin = PhaseSpaceVisualizer(df_thin, label="Resampled data")
+        phase_space_thin.create_plot().savefig("phase_space.png")
 
-    # Write the reduced dataframe to a file
-    DataFrameToFile(df_thin).exclude_weights().exclude_energy().write_to_file(
-        opmd_path.with_suffix(".txt")
-    )
+    if not no_save:
+        DataFrameToFile(df_thin).exclude_weights().exclude_energy().write_to_file(
+            opmd_path.with_suffix(".txt")
+        )
 
 
 if __name__ == "__main__":
